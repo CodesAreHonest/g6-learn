b296ed2447ffd5f7abd5a6f3947ba614713bddd2
Yinghua Chai <yinghuachai0@gmail.com>
2020-12-24 14:45:05 +0800

    chore: commit something to trigger e2e test

--- src/GettingStarted/GettingStarted.tsx
+++ src/GettingStarted/GettingStarted.tsx
@@ -55,7 +55,13 @@ const GettingStarted = () => {
     graphRef.current.render();
   }, []);
 
-  return <div ref={containerRef} id="getting-started" />;
+  return (
+    <div
+      ref={containerRef}
+      id="getting-started"
+      data-testid="getting-started"
+    />
+  );
 };
 
 export default GettingStarted;
